e4b309fb8ed66e5425599d1d12abf51c5845e6eb
Daniel <ardekanicapital@gmail.com>
2020-07-30 02:24:55 -0400

    convert inches to meters and lbs to kilos. ask user for weight and height

---
+++ da2753_hw3_q5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double weight, height, bmi, in_to_m, lbs_to_kgs;
+
+    cout << "Please enter weight (in pounds): ";
+    cin >> weight;
+    cout << "Please enter height (in inches): ";
+    cin >> height
+
+    lbs_to_kilos = weight * 0.453592;
+    in_to_m = height * 0.0254;
+
+    bmi = lbs_to_kilos / in_to_m
+
+}
